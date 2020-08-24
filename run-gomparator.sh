65251dd7677a2673213f6605fcb914ca009d184a
Mauro <mauro.pons@mercadolibre.com>
2020-08-24 09:32:20 -0300

    changed defaults

--- run-gomparator.sh
+++ run-gomparator.sh
@@ -8,19 +8,13 @@ AUTH_TOKEN="b1911bf99c5c32b0c151095a9f956ece09801dc3eaaa966a0e2911e23df7c4a8"
 SCOPE_1="https://read-batch_payment-methods.furyapps.io"
 SCOPE_2="https://production-reader-testscope_payment-methods-read-v2.furyapps.io"
 ARRAY_PATHS=(
-	"/Users/mpons/Documents/comparator/payment-methods/v2/17-08-2020_21-08-2020/202008-10-15/MELI/MLM/MLM.csv"
-	"/Users/mpons/Documents/comparator/payment-methods/v2/17-08-2020_21-08-2020/202008-10-15/MELI/MCO/MCO.csv"
-	"/Users/mpons/Documents/comparator/payment-methods/v2/17-08-2020_21-08-2020/202008-10-15/NONE/MLM/MLM.csv"
-	"/Users/mpons/Documents/comparator/payment-methods/v2/17-08-2020_21-08-2020/202008-10-15/NONE/MCO/MCO.csv"
+	"/Users/mpons/Documents/comparator/payment-methods/v2/2_24-08-2020_28-08-2020/202008-10-15/MELI/MLM/MLM.error"
+	"/Users/mpons/Documents/comparator/payment-methods/v2/2_24-08-2020_28-08-2020/202008-10-15/MELI/MCO/MCO.error"
+	"/Users/mpons/Documents/comparator/payment-methods/v2/2_24-08-2020_28-08-2020/202008-10-15/NONE/MLM/MLM.error"
+	"/Users/mpons/Documents/comparator/payment-methods/v2/2_24-08-2020_28-08-2020/202008-10-15/NONE/MCO/MCO.error"
 	) 
 
-ARRAY_CHANNELS=("point" "splitter" "instore")
-
-for i in "${ARRAY_PATHS[@]}"
-do
-	gomparator -path "$i" -host "${SCOPE_1}" -host "${SCOPE_2}" -header "X-Auth-Token:${AUTH_TOKEN}"
-done
-
+ARRAY_CHANNELS=("" "point" "splitter" "instore")
 for i in "${ARRAY_PATHS[@]}"
 do
 	for j in "${ARRAY_CHANNELS[@]}"
